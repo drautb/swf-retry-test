189ca7cdb9e73242eaa5f3cb11ff29ae33bd0896
Ben Draut <drautb@gmail.com>
2019-01-14 12:15:46 -0700

    Step 1 - Throw R53 Exception from activity
    This shows that the activity throws the exception, it gets wrapped in
    an internal exception, with the orginal in the `details` field in
    string form.
    
    The next step will be to remove the R53 SDK from the decider, and see
    if we get the same behavior.

--- activity/src/main/java/io/github/drautb/swf/activity/impl/ActivityImpl.java
+++ activity/src/main/java/io/github/drautb/swf/activity/impl/ActivityImpl.java
@@ -1,5 +1,6 @@
 package io.github.drautb.swf.activity.impl;
 
+import com.amazonaws.services.route53.model.AmazonRoute53Exception;
 import com.amazonaws.services.simpleworkflow.flow.ActivityExecutionContext;
 import com.amazonaws.services.simpleworkflow.flow.ActivityExecutionContextProviderImpl;
 import io.github.drautb.swf.activity.api.Activity;
@@ -9,17 +10,19 @@ import org.familysearch.paas.sps.common.annotations.AdaptExceptionForSwf;
 public class ActivityImpl implements Activity {
 
   public void spin() {
-    for (int n=0; n<10; n++) {
-      try {
-        Thread.sleep(1000);
-      }
-      catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
+//    for (int n=0; n<10; n++) {
+//      try {
+//        Thread.sleep(1000);
+//      }
+//      catch (InterruptedException e) {
+//        throw new RuntimeException(e);
+//      }
+//
+//      ActivityExecutionContext context = new ActivityExecutionContextProviderImpl().getActivityExecutionContext();
+//      context.recordActivityHeartbeat("sleeping");
+//    }
 
-      ActivityExecutionContext context = new ActivityExecutionContextProviderImpl().getActivityExecutionContext();
-      context.recordActivityHeartbeat("sleeping");
-    }
+    throw new AmazonRoute53Exception("test r53 error");
   }
 
 }
