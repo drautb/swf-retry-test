15601ede7ecfad1a59396ee64daf1eed2d5e5055
Ben Draut <drautb@gmail.com>
2020-01-07 12:57:16 -0700

    Testing AWS lock client

--- swf-retry-test/src/main/java/io/github/drautb/swf/test/impl/ActivityImpl.java
+++ swf-retry-test/src/main/java/io/github/drautb/swf/test/impl/ActivityImpl.java
@@ -1,5 +1,6 @@
 package io.github.drautb.swf.test.impl;
 
+import com.amazonaws.services.dynamodbv2.*;
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflowClientBuilder;
 import com.amazonaws.services.simpleworkflow.flow.ActivityExecutionContext;
 import com.amazonaws.services.simpleworkflow.flow.ActivityExecutionContextProviderImpl;
@@ -10,6 +11,9 @@ import io.github.drautb.swf.test.api.WorkflowOneDeciderClientExternalFactory;
 import io.github.drautb.swf.test.api.WorkflowOneDeciderClientExternalFactoryImpl;
 import org.familysearch.paas.sps.common.annotations.AdaptExceptionForSwf;
 
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
 @AdaptExceptionForSwf
 public class ActivityImpl implements Activity {
 
@@ -19,17 +23,32 @@ public class ActivityImpl implements Activity {
           SwfRetryTest.DOMAIN);
 
   public void spin() {
-    for (int n=0; n<10; n++) {
+      AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.standard().withRegion("us-east-1").build();
+
+      AmazonDynamoDBLockClient client = new AmazonDynamoDBLockClient(AmazonDynamoDBLockClientOptions.builder(dynamoDB, "adhoc-drautb-test")
+//          .withCreateHeartbeatBackgroundThread(true)
+          .withHeartbeatPeriod(3L)
+          .withLeaseDuration(10L)
+//          .withOwnerName("test-owner")
+          .withTimeUnit(TimeUnit.SECONDS)
+          .withHoldLockOnServiceUnavailable(false)
+          .withPartitionKeyName("lock_name")
+          .build());
+
       try {
-        Thread.sleep(1000);
+        client.assertLockTableExists();
+        LockItem lock = client.acquireLock(AcquireLockOptions.builder("this-is-the-lock").build());
+        int i = 42;
+        System.out.println(i);
+        System.out.println(lock);
+//        LockItem lock = client.acquireLock(AcquireLockOptions.builder("this-is-the-lock").build());
+//        client.tryAcquireLock()
+
       }
       catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
 
-      ActivityExecutionContext context = new ActivityExecutionContextProviderImpl().getActivityExecutionContext();
-      context.recordActivityHeartbeat("sleeping");
-    }
   }
 
   public void signal() {
